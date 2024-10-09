d31e498a93431055a67fc266b1a4d8379cf5ca0a
stk346 <stk346@gmail.com>
2024-10-09 14:07:09 +0900

    feat: getMovieDetailInfo() 메서드 추가 및 getMovieInfo() 메서드에서 영화 코드 정보 가져오는 코드 추가, 예외처리 추가

--- src/main/java/com/polar_moviechart/scrapingservice/domain/service/kobis/moviedata/DataExtractor.java
+++ src/main/java/com/polar_moviechart/scrapingservice/domain/service/kobis/moviedata/DataExtractor.java
@@ -1,10 +1,14 @@
 package com.polar_moviechart.scrapingservice.domain.service.kobis.moviedata;
 
+import com.polar_moviechart.scrapingservice.exception.ScrapingDataNotFoundException;
 import com.polar_moviechart.scrapingservice.utls.DataExtractUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class DataExtractor {
@@ -15,6 +19,46 @@ public class DataExtractor {
         int sales = DataExtractUtils.convertToInt(columns.get(3).getText());
         int audience = DataExtractUtils.convertToInt(columns.get(7).getText());
 
-        return new MovieBasicInfoDto(rank, title, sales, audience);
+        WebElement linkElement = columns.get(1).findElement(By.cssSelector("a"));
+        String onClickValue = linkElement.getAttribute("onClick");
+        int code = extractMovieCode(onClickValue);
+
+        MovieBasicInfoDto movieBasicInfoDto = new MovieBasicInfoDto(code, rank, title, sales, audience);
+
+        return new MovieBasicInfoDto(code, rank, title, sales, audience);
+    }
+
+    public void getMovieDetailInfo(WebElement movieDetailPage, MovieBasicInfoDto basicInfo) {
+        // 영화를 클릭하면 나오는 상세 정보 템플릿에서 영화 메타데이터 가져오기
+        String titleEnglish = movieDetailPage.findElement(By.cssSelector("div.hd_layer > div"))
+                .getText()
+                .replace("영화상영관상영중", "")
+                .trim();
+        String title = basicInfo.getTitle() + " " + titleEnglish;
+
+        WebElement movieInfo = movieDetailPage.findElement(By.cssSelector("dl.ovf"));
+        String synopsys = movieDetailPage.findElement(By.cssSelector("p.desc_info")).getText();
+
+        List<WebElement> metadata = movieInfo.findElements(By.cssSelector("dt, dd"));
+        String code = metadata.get(1).getText();
+        String movieDetails = metadata.get(7).getText();
+        String releaseDate = metadata.get(11).getText();
+        String productionYear = metadata.get(13).getText();
+    }
+
+    private int extractMovieCode(String onClickValue) {
+        // 정규 표현식 패턴
+        Pattern pattern = Pattern.compile("mstView\\('movie','(\\d+)'\\)");
+        Matcher matcher = pattern.matcher(onClickValue);
+
+        // 코드 찾기
+        try {
+            if (matcher.find()) {
+                return Integer.parseInt(matcher.group(1));
+            }
+        } catch (Exception e) {
+            throw new ScrapingDataNotFoundException("영화 코드 추출 중 예외 발생.", e);
+        }
+        throw new ScrapingDataNotFoundException("코드를 찾을 수 없습니다.");
     }
 }
