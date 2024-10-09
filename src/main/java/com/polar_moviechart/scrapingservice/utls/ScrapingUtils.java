0b6428b8ba0e7a29f59f1c646a6077a2d3e231f3
stk346 <stk346@gmail.com>
2024-10-09 12:10:13 +0900

    feat: ScrapingUtils - 스크래핑할 때 공통적으로 사용할 메서드 사용할 클래스 추가

---
+++ src/main/java/com/polar_moviechart/scrapingservice/utls/ScrapingUtils.java
@@ -0,0 +1,16 @@
+package com.polar_moviechart.scrapingservice.utls;
+
+public class ScrapingUtils {
+
+    // private 생성자를 추가하여 인스턴스 생성을 방지
+    private ScrapingUtils() {
+        throw new UnsupportedOperationException("Utility class cannot be instantiated");
+    }
+
+    public static int convertToInt(String numberString) {
+        if (numberString == null || numberString.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(numberString.replace(",", ""));
+    }
+}
