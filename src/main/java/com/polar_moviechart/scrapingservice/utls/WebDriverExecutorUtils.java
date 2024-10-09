085124fefc1950be697a99fe9e0992a9089f3c68
stk346 <stk346@gmail.com>
2024-10-09 12:36:18 +0900

    feat: WebDriverExecutorUtils - 웹 페이지를 이동할 때 공통적으로 쓰이는 클래스 추가

---
+++ src/main/java/com/polar_moviechart/scrapingservice/utls/WebDriverExecutorUtils.java
@@ -0,0 +1,17 @@
+package com.polar_moviechart.scrapingservice.utls;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class WebDriverExecutorUtils {
+
+    private WebDriverExecutorUtils() {
+        throw new UnsupportedOperationException("Utility class cannot be instantiated");
+    }
+
+    public static void doEsc(WebDriver driver) {
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.ESCAPE).perform();
+    }
+}
