2d010e1affda644ba33cbc862f55fb42435375d0
stk346 <stk346@gmail.com>
2024-10-11 12:56:29 +0900

    feat: Movie, MovieDailyStats - 테이블, 엔티티 정보 추가

--- src/main/java/com/polar_moviechart/scrapingservice/domain/entity/Movie.java
+++ src/main/java/com/polar_moviechart/scrapingservice/domain/entity/Movie.java
@@ -1,16 +1,30 @@
 package com.polar_moviechart.scrapingservice.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 @Entity
+@Table(name = "movies")
 public class Movie {
-    private final int id;
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
     private final int code;
+
+    @Column(nullable = false)
     private final String title;
 
+    @OneToMany(mappedBy = "movie")
+    private final List<MovieDailyStats> stats = new ArrayList<>();
+
     public Movie(int id, int code, String title) {
-        this.id = id;
         this.code = code;
         this.title = title;
     }
--- src/main/java/com/polar_moviechart/scrapingservice/domain/entity/MovieDailyStats.java
+++ src/main/java/com/polar_moviechart/scrapingservice/domain/entity/MovieDailyStats.java
@@ -1,13 +1,31 @@
 package com.polar_moviechart.scrapingservice.domain.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "movie_daily_stats")
 public class MovieDailyStats {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
     private final int code;
+
+    @Column(nullable = false)
     private final int rank;
+
+    @Column(nullable = false)
     private final int revenue;
+
+    @Column(nullable = false)
     private final int audience;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
 
     public MovieDailyStats(int code, int rank, int revenue, int audience) {
         this.code = code;
