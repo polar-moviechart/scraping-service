7b5b523b2acdfe1fa259cfa495db18ed2b974142
stk346 <stk346@gmail.com>
2024-10-14 02:03:22 +0900

    feat: 메인 클래스에서 날짜 입력해 스크래핑 하는 코드 추가

--- src/main/java/com/polar_moviechart/scrapingservice/ScrapingServiceApplication.java
+++ src/main/java/com/polar_moviechart/scrapingservice/ScrapingServiceApplication.java
@@ -1,13 +1,30 @@
 package com.polar_moviechart.scrapingservice;
 
+import com.polar_moviechart.scrapingservice.domain.service.kobis.moviedata.ScrapingScheduler;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScrapingServiceApplication {
+
+	private final ScrapingScheduler scrapingScheduler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrapingServiceApplication.class, args);
 	}
 
+	public ScrapingServiceApplication(ScrapingScheduler scrapingScheduler) {
+		this.scrapingScheduler = scrapingScheduler;
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return (args) -> {
+			String startDate = "2004-01-01";
+			String endData = "2004-01-31";
+			scrapingScheduler.doScrape(startDate, endData);
+		};
+	}
 }
