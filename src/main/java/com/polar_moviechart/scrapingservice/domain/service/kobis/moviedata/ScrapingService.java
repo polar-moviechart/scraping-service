f8422ddf26882afaf5c9cc775fd0bac58b7adee9
stk346 <stk346@gmail.com>
2024-10-15 00:28:41 +0900

    feat: ScrapingServie - null 방어를 위한 코드 리팩터링 및 트랜잭션 설정

--- src/main/java/com/polar_moviechart/scrapingservice/domain/service/kobis/moviedata/ScrapingService.java
+++ src/main/java/com/polar_moviechart/scrapingservice/domain/service/kobis/moviedata/ScrapingService.java
@@ -1,14 +1,17 @@
 package com.polar_moviechart.scrapingservice.domain.service.kobis.moviedata;
 
 import com.polar_moviechart.scrapingservice.domain.entity.Movie;
+import com.polar_moviechart.scrapingservice.domain.entity.MovieDailyStats;
 import com.polar_moviechart.scrapingservice.domain.repository.MovieRepository;
 import com.polar_moviechart.scrapingservice.domain.service.*;
 import com.polar_moviechart.scrapingservice.domain.service.kobis.moviedata.extractor.DataExtractor;
 import com.polar_moviechart.scrapingservice.domain.service.kobis.moviedata.processor.MovieProcessor;
 import com.polar_moviechart.scrapingservice.utls.DataExtractUtils;
 import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +27,7 @@ public class ScrapingService {
     private final MovieProcessor movieProcessor;
     private final MovieDailyStatsQueryService movieDailyStatsQueryService;
 
+    @Transactional
     public void doScrape(String targetDate) {
         webDriverExecutor.navigateToPage(targetDate);
         preparePage();
@@ -34,20 +38,24 @@ public class ScrapingService {
         }
     }
 
+    @Transactional
     private void processMovieRow(WebElement row, String targetDate) {
         List<WebElement> columnInfo = webDriverExecutor.getColumnInfo(row);
         MovieDailyStatsDto movieDailyStatsDto = dataExtractor.getMovieDailyStatsInfo(columnInfo);
 
-        Optional<Movie> movieCodeOptional = movieRepository.findByCode(movieDailyStatsDto.getCode());
-        Movie movie = null;
-        if (movieCodeOptional.isEmpty()) {
+        Optional<Movie> movieOptional = movieRepository.findByCode(movieDailyStatsDto.getCode());
+        Movie movie = movieOptional.orElseGet(() -> {
             WebElement movieDetailPage = webDriverExecutor.moveToMovieDetailPage(row);
-            movie = movieProcessor.processNewMovie(movieDetailPage, movieDailyStatsDto);
-        }
+            Movie createdMovie = movieProcessor.processNewMovie(movieDetailPage, movieDailyStatsDto);
+            movieDetailPage.findElement(By.cssSelector("div.hd_layer > a.close")).click();
+            return createdMovie;
+        });
 
         boolean existsByCodeAndDate = movieDailyStatsQueryService.isExists(movieDailyStatsDto.getCode(), DataExtractUtils.convertToLocalDate(targetDate));
         if (!existsByCodeAndDate) {
-            movieDailyStatsCommandService.save(movie, movieDailyStatsDto, DataExtractUtils.convertToLocalDate(targetDate));
+            MovieDailyStats movieDailyStats = movieDailyStatsDto.toEntity(DataExtractUtils.convertToLocalDate(targetDate));
+            movieDailyStats.setMovie(movie);
+            movieDailyStatsCommandService.save(movieDailyStats);
         }
     }
 
